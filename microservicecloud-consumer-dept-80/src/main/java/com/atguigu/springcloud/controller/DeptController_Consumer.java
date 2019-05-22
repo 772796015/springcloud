77cc794fc904fe9c8319ccb33a6c9660d9b44118
772796015 <772796015@qq.com>
2019-05-22 17:28:09 +0800

    增加备注---访问消费者地址已经eureka地址备注

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -8,7 +8,11 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class DeptController_Consumer
 {
+//页面调用该消费者地址：http://localhost/consumer/hello
 
+	
+
+//调用eureka服务地址
 //	private static final String REST_URL_PREFIX = "http://localhost:8001";
 	private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
 
