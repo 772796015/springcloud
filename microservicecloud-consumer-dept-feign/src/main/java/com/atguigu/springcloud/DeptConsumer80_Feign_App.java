0ec6bac1f17d854b156356c005ebb07ae7b31f23
772796015 <772796015@qq.com>
2019-05-28 10:32:53 +0800

    增加注释

--- microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -5,6 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+
+//浏览器访问地址 http://localhost/consumer/hello
+//总结 改造基本在消费端     microservicecloud-consumer-dept-feign
+//测试此机制时需启动服务如下： 服务消费者：microservicecloud-consumer-dept-feign,eureka服务：microservicecloud-eureka-7001、服务提供者：microservicecloud-provider-dept-8001,8002,8003
+
+//1.基于microservicecloud-consumer-dept-80修改为接口调用方式,feign基于Ribbon所以需要引用其所需的pom坐标
+/*<!-- feign -->
+		<dependency>
+			<groupId>org.springframework.cloud</groupId>
+			<artifactId>spring-cloud-starter-feign</artifactId>
+		</dependency>
+
+ * */
+//2. @EnableFeignClients
+//3.新写service接口  添加@FeignClient(name = "MICROSERVICECLOUD-DEPT")注解，name为需要调用服务的名称
+
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
