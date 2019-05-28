9cf20f145501b99c7db633dd0ff6077d4b3aaa75
772796015 <772796015@qq.com>
2019-05-28 10:33:23 +0800

    新增注释

--- microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -4,6 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/*
+ * 1.引入坐标<!--eureka-server服务端 -->
+		<dependency>
+			<groupId>org.springframework.cloud</groupId>
+			<artifactId>spring-cloud-starter-eureka-server</artifactId>
+		</dependency>
+	2.	@EnableEurekaServer
+	3.配置application.yml
+ */
+
+
+
 @SpringBootApplication
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
 public class EurekaServer7001_App {
